78c6ee9bce69ae822c44d17868e1e609c7aef5a7
Josh <joshua.sankarlal@ontariotechu.net>
2021-01-31 15:43:29 -0500

    Gravity?

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -6,30 +6,48 @@ using UnityEngine.SceneManagement;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
-    //public Transform cam;
+    public Transform cam;
+
     public float speed = 6f;
+    public float gravity = -9.81f;
+
     public float turnSmoothTime = 0.1f;
-    public bool hasKey = false;
     float turnSmoothVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool hasKey;
+    bool isGrounded;
+
     Scene scene;
-    void Awake()
-    {
-    }
+    Vector3 velocity;
+    // Update is called once per frame
 
     void Start()
     {
-        scene = SceneManager.GetActiveScene();
-        if (scene.name == "Level 1")
-            hasKey = true;
+      scene = SceneManager.GetActiveScene();
+      if (scene.name == "Level 1")
+      {
+          hasKey = true;
+      }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if (isGrounded && velocity.y < 0 )
+        {
+            velocity.y = -2f;
+        }
+        
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
-
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
         //Getting some input to move
         if (dir.magnitude >= 0.1f)
         {
@@ -38,17 +56,14 @@ public class ThirdPersonMovement : MonoBehaviour
             //Smooths the rotation
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
             //Direction we want to move in
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);      
         }
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-       
 
         if (hit.transform.tag == "Key")
         {
