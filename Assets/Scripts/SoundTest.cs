4f5b9b67c0f377248b59218ef9c7596fdb1eb3ad
Carter Menary <carter.menary@gmail.com>
2021-01-31 18:48:35 -0500

    sdfjkl

--- Assets/Scripts/SoundTest.cs
+++ Assets/Scripts/SoundTest.cs
@@ -5,41 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class SoundTest : MonoBehaviour
 {
+    
     private FMODUnity.StudioEventEmitter emitter;
+    private FMOD.Studio.EventInstance instance;
 
     // Start is called before the first frame update
     void Start()
     {
+        //DontDestroyOnLoad(gameObject);
         emitter = GetComponent<FMODUnity.StudioEventEmitter>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Alpha1)) {
+        //SceneManager.sceneLoaded
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
             emitter.SetParameter("Level", 1);
         }
-        if (Input.GetKey(KeyCode.Alpha2)) {
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
             emitter.SetParameter("Level", 2);
         }
-        if (Input.GetKey(KeyCode.Alpha3)) {
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            Debug.Log("FUCK MY ASSHOLE BITCHASS HOE FROG");
             emitter.SetParameter("Level", 3);
+            Debug.Log("Parameter set");
+            
         }
-        if (Input.GetKey(KeyCode.Alpha4)) {
+        if (Input.GetKeyDown(KeyCode.Alpha4)) {
             emitter.SetParameter("Level", 4);
         }
-        if (Input.GetKey(KeyCode.Alpha5)) {
+        if (Input.GetKeyDown(KeyCode.Alpha5)) {
             emitter.SetParameter("Level", 5);
         }
-        if (Input.GetKey(KeyCode.Alpha6)) {
+        if (Input.GetKeyDown(KeyCode.Alpha6)) {
             emitter.SetParameter("Level", 6);
         }
-        if (Input.GetKey(KeyCode.Alpha7)) {
+        if (Input.GetKeyDown(KeyCode.Alpha7)) {
             emitter.SetParameter("Level", 7);
         }
 
         if (Input.GetKey(KeyCode.Space)) {
-            SceneManager.LoadScene("Level 1");
+            SceneManager.LoadScene("SoundTest2");
         }
     }
+        
 }
