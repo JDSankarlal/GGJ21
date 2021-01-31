7364bef36a34c30fa6dac6f61046d4ac4f474b89
Josh <joshua.sankarlal@ontariotechu.net>
2021-01-31 17:00:01 -0500

    Trying our fucking best

---
+++ Assets/PlatformScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+
+    public Vector3 targetPos1, targetPos2;
+    public Rigidbody rb;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        rb.MovePosition(transform.position + -Vector3.up * Time.deltaTime * speed);
+
+
+        
+    }
+}
